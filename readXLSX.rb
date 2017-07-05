619b0f9d6f0031b9681b422a4194c4bafe8467a0
Victor Tavares <victor.tavares@itelios.com>
2017-07-05 13:52:27 -0300

    Adjustments.

--- readXLSX.rb
+++ readXLSX.rb
@@ -1,10 +1,10 @@
 require 'rubyXL'
 
 fileName = ARGV[0]
-sheet = ARGV[1]
-prefix = ARGV[2]
-colum = ARGV[3]
-country = ARGV[4]
+prefix = ARGV[1]
+colum = ARGV[2]
+country = ARGV[3]
+sheet = ARGV[4]
 
 workbook = RubyXL::Parser.parse(fileName)
 worksheet = workbook.worksheets[sheet.to_i]
@@ -12,12 +12,13 @@ worksheet = workbook.worksheets[sheet.to_i]
 goon = true
 index = 1
 
-my_file = File.new("properties_"+ country +".txt", "w+")
+my_file = File.new("capaddresse_"+ country +".properties", "a")
 
 while goon
-    if worksheet[index][colum.to_i]
+    if worksheet[index] && worksheet[index][colum.to_i]
+        errornumber = worksheet[index][0]
         cell = worksheet[index][colum.to_i]
-        my_file.write(prefix + "." + index.to_s + "=" + cell.value + "\n")
+        my_file.write(prefix + "." + errornumber.value.to_s + "=" + cell.value + "\n")
     else
         goon = false
     end
